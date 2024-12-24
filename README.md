4512c731cda485a324dd3355e8d0130337667a96
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-12-24 16:49:14 +0400

    updated README and some of info

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 
 This website template, brought to you by Web Canvas, is free to use and fully customizable. Follow the steps below to personalize it to your needs.
 
+**Disclaimer**
+
+*Web Canvas provides these templates free of charge and ensures they are safe and free from malicious content at the time of delivery. However, we are not responsible for any issues, damages, or unlawful activities arising from the use or misuse of these templates by the user. By using our templates, you agree to take full responsibility for their implementation and usage.*
+
 ### What You Need to Do:
 
 Each step is accompanied by detailed comments and explanations within the relevant files to guide you through the process.
 
 1. **Update the Navbar**:
+
    - Open `Navbar.jsx`.
    - Locate the `<h1>` tag and replace "Name" with your real name.
 
 2. **Personalize ShortInfo**:
+
    - Open `ShortInfo.jsx`.
    - Replace "Name Surname" with your full name.
    - Replace "Your Position" with your current role (e.g., MERN Stack Developer or Front-End Developer).
@@ -22,6 +28,7 @@ Each step is accompanied by detailed comments and explanations within the releva
    - Update years of experience and the number of completed projects.
 
 3. **Customize Service Cards**:
+
    - Open `Services.jsx`.
    - Modify the predefined components by updating the following props:
      - `icon`
@@ -30,15 +37,18 @@ Each step is accompanied by detailed comments and explanations within the releva
    - Follow the inline comments to guide your changes.
 
 4. **Edit About Me Section**:
+
    - Open `AboutMe.jsx`.
    - Update the "About Me" and "Why Me" texts.
    - Modify the skill cards by changing the names and icons using the predefined components.
 
 5. **Update Portfolio Projects**:
+
    - Open `Portfolio.jsx`.
    - Edit the predefined project components by updating their props to showcase your work.
 
 6. **Configure the Contact Form**:
+
    - Open `ContactMe.jsx`.
    - Update the form submission URL. This template uses [Formsubmit.co](https://formsubmit.co/) by default, but you can replace it with another service.
 
@@ -62,6 +72,10 @@ Each step is accompanied by detailed comments and explanations within the releva
 ---
 
 ## Thank You for Using Our Templates
+
 We hope this template meets your expectations. We’d love to hear your feedback about our work.
+
+**Also Please Note that we aren't responsible for the your mistakes and anything other when you start using our templates**
+**They are completly free and safe to use**
 
 **By Web Canvas**
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -25,84 +25,38 @@ const Portfolio = () => {
 
       <div className="portfolio-list">
         <ProjectsCard
-        // In icon place you can place react component or html tag.
+          // In icon place you can place react component or html tag.
           icon={<img style={{ width: "70px" }} src={logo} alt="logo" />}
           // title will change the header of the project card
-          title={"Web Canvas"}
+          title={"Project Name"}
           // In here you can write the status of your project such as: In progress, paused, completed and so on.
           status={"In Progress"}
           // A short description of your project.
-          description={`My Own Company Web Canvas is an emerging startup offering a unique
-          online platform, offering fully ready made websites.`}
+          description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis dolorum `}
           // Write the url of your project.
-          URL={"https://webcanvasgeo.netlify.app/"}
+          URL={"#"}
           fadeSide={"fade-right"}
           fadeDelay={100}
         />
 
         <ProjectsCard
           icon={<CiShop size={50} color="white" />}
-          title={"MelinaMShop"}
+          title={"Project Name"}
           status={"Completed"}
-          description={`A website for selling restaurant products and more, made on MERN
-            Stack
-            Launched in 01.05.2024`}
-          URL={"https://melinamshop.netlify.app/"}
+          description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis dolorum `}
+          URL={"#"}
           fadeSide={"fade-up"}
           fadeDelay={200}
         />
 
         <ProjectsCard
           icon={<MdModeOfTravel size={50} color="white" />}
-          title={"TravelPulse"}
+          title={"Project Name"}
           status={"Paused"}
-          description={`Here you can share and view a stories about travel beautiful places
-            and alot more, made on MERN Stack`}
-          URL={"https://travelpulse.netlify.app/"}
+          description={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae debitis dolorum `}
+          URL={"#"}
           fadeSide={"fade-left"}
           fadeDelay={300}
-        />
-
-        <ProjectsCard
-          icon={<LuEarth size={50} color="white" />}
-          title={"Planets Fact Site"}
-          status={"Completed"}
-          description={`Here You will see a facts about planets designed with comfortable
-            UI/UX`}
-          URL={"https://planetsfactfromgio.netlify.app/"}
-          fadeSide={"fade-right"}
-          fadeDelay={400}
-        />
-
-        <ProjectsCard
-          icon={<GrLanguage size={50} color="white" />}
-          title={"GeorgiaProCode"}
-          status={"Paused"}
-          description={`A Website with free programming courses for childrens from 10 years
-            and other ages at georgian`}
-          URL={"https://georgianprocode.netlify.app/"}
-          fadeSide={"fade-up"}
-          fadeDelay={500}
-        />
-
-        <ProjectsCard
-          icon={<GiTicTacToe size={50} color="white" />}
-          title={"Tic Tac Toe"}
-          status={"Completed"}
-          description={`Tic Tac Toe Game, you can play it with your friends, enjoy the game.`}
-          URL={"https://tictactoefromgio.netlify.app/"}
-          fadeSide={"fade-left"}
-          fadeDelay={550}
-        />
-
-        <ProjectsCard
-          icon={<SlCalculator size={50} color="white" />}
-          title={"Calculator"}
-          status={"Completed"}
-          description={`A mini version of calculator, which can perform basic math operations.`}
-          URL={"https://calculatorfromgio.netlify.app/"}
-          fadeSide={"fade-up"}
-          fadeDelay={550}
         />
       </div>
     </div>
